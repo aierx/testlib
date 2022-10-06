a133827a8b893b6e1b5c1b13ff100aa91b5380b7
leiwwenyong <aleiwenyong@outlook.com>
2022-10-06 23:11:56 +0800

    a

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace TestLib
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("初始化");
+        }
+
+        public void showMe()
+        {
+            Console.WriteLine("hello world");
+        }
+
+    }
+}
